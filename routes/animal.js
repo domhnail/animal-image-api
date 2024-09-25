e6f58fbd58db642d7f3f00f39353eb6bf5e9e347
domhnail <coledanodonnell@hotmail.com>
2024-09-25 11:09:22 -0300

    Status messages, working on fixing orphaned images.

--- routes/animal.js
+++ routes/animal.js
@@ -1,8 +1,10 @@
 import express from 'express';
 import multer from 'multer';
 import { PrismaClient } from '@prisma/client';
+import fs from 'fs';
 
 const router = express.Router();
+
 
 //prisma setup (logging)
 const prisma = new PrismaClient ({
@@ -32,6 +34,7 @@ router.get('/', (req, res) => {
 router.get('/all', async (req, res) => {
   const image = await prisma.image.findMany();
   
+  res.status(200).json({message: 'Displaying all image entries.'});
   res.json(image);
 });
 
@@ -48,18 +51,27 @@ router.get('/read/:id', async (req, res) => {
       id: parseInt(id),
     },
   });
-
+  
+  //404 when image not found
   if (!image) {
-    res.status(404).send('Image not found.');
+    res.status(404).json({message: 'Image not found.'});
   } else {
+    res.status(200).json({message: `Image with id: ${id} found.`})
     res.json(image);
   }
 });
 
 //TODO: there's something up with validation here, where if I upload falsely (missing a value in the req.body), it still uploads the image and increments the ID
+//run a delete on uploaded file and filename if post fails
+//TODO: validate file extensions
+//could do switch case for each given null?
 //ADDING NEW IMAGES
 // .../api/photo/create
 router.post('/create', upload.single('image'), async (req, res) => {
+
+  //for deleting failed upload files
+  let failedUpload = function () {
+  }
   
   //setting filename to blank if no file was uploaded
   const filename = req.file ? req.file.filename: '';
@@ -81,24 +93,31 @@ router.post('/create', upload.single('image'), async (req, res) => {
       },
     });
     //presenting results
+    res.status(200).json({message: 'Image successfully uploaded, database entry created.'});
     res.json(image);
-  } else {
-    return res.status(400).send('One or more fields were not filled in.');
+  } else { //400 when there are null fields submitted
+    fs.unlink(`public/images/${filename}`,failedUpload);
+    return res.status(400).json({message: 'One or more fields were not filled in.'});
   }
 });
 
 //TODO: validation, run a delete on the previous file when changing images out
+//could do switch case for each given null?
+//TODO: image deletion if a new image is uploaded, checking if an image was uploaded in the first place
+// rec was to make a blank filename variable to assign the new or old image filename to
 //UPDATING IMAGES
 // .../api/photo/update
 router.put('/update', upload.single('image'), async (req, res) => {
 
+  const id = req.params.id;
+
   const filename = req.file ? req.file.filename: '';
 
   //receiving updated image description and tags etc.
-  const {id,image_name,description,tags,animal,image_size,genre} = req.body;
+  const {image_name,description,tags,animal,image_size,genre} = req.body;
 
-  //updating record
-  if (image_name && description && tags && animal && image_size && genre) {
+  //updating record (replace && with ||'s? would make it so that just at least one field has to be filled to update)
+  if (image_name || description || tags || animal || image_size || genre) {
     const updateImage = await prisma.image.update({
       //searching by the id
       where: {
@@ -115,9 +134,10 @@ router.put('/update', upload.single('image'), async (req, res) => {
         filename: filename
       },
     })
-    res.send(updateImage);
-  } else {
-    return res.status(400).send('One or more fields were not filled in.');
+    res.status(200).json({message: 'Image updated.'});
+    res.json(updateImage);
+  } else { //400 when fields aren't filled in (need to adjust this actually)
+    return res.status(400).json({message: 'One or more fields were not filled in.'});
   }
 });
 
@@ -135,7 +155,11 @@ router.delete('/delete/:id', async (req, res) => {
       id: parseInt(id),
     },
   });
+  
+  fs.unlink(req.image.path);
   res.json(deleteImage);
 });
+
+//TODO: batch process to clean up orphaned images
 
 export default router;
